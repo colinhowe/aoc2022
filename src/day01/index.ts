5c7420337bdbc7f919a09fd1c8b41c7b36be6de7
Colin Howe <88904+colinhowe@users.noreply.github.com>
2022-12-02 09:12:49 +0000

    Day 1 Part 1

--- src/day01/index.ts
+++ src/day01/index.ts
@@ -1,11 +1,21 @@
 import run from "aocrunner";
 
-const parseInput = (rawInput: string) => rawInput;
+const parseInput = (rawInput: string) => {
+  return rawInput.split("\n").map(s => s.length ? Number.parseInt(s, 10) : undefined);
+}
 
 const part1 = (rawInput: string) => {
   const input = parseInput(rawInput);
+  const [overallMax] = input.reduce(([lastMax, sumSoFar], nextInput) => {
+    if (nextInput) {
+      const newSum = sumSoFar + nextInput;
+      return [Math.max(newSum, lastMax), newSum];
+    } else {
+      return [lastMax, 0];
+    }
+  }, [0, 0]);
 
-  return;
+  return overallMax.toString();
 };
 
 const part2 = (rawInput: string) => {
@@ -17,10 +27,23 @@ const part2 = (rawInput: string) => {
 run({
   part1: {
     tests: [
-      // {
-      //   input: ``,
-      //   expected: "",
-      // },
+      {
+        input: `1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000`,
+        expected: "24000",
+      },
     ],
     solution: part1,
   },
